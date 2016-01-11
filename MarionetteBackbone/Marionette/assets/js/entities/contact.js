5c58d66add0b703280720861ed775351ac9fd61d
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-11 11:56:57 +0000

    Modules added.

---
+++ MarionetteBackbone/Marionette/assets/js/entities/contact.js
@@ -0,0 +1,24 @@
+﻿ContactManager.module("Entities", function (Entities, ContactManager, Backbone, Marionette, $, _) {
+  //var alertPrivate = function (message) {
+  //  alert("Private alert: " + message);
+  //};
+
+  //Entities.alertPublic = function (message) {
+  //  alert("I will now call alertPrivate" + message);
+  //}
+
+  Entities.Contact = Backbone.Model.extend({
+    defaults: {
+      //firstName: "Filip",
+      //phoneNumber: "No number"
+    }
+  });
+
+  Entities.ContactCollection = Backbone.Collection.extend({
+    model: Entities.Contact,
+    comparator: function (item) {
+      return [item.get("firstName"), item.get("lastName")];
+    }
+  });
+
+});
