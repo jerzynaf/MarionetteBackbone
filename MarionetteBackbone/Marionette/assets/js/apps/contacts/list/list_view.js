e2021e5c72e55222210f759cf41287b6db2f1ef6
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-11 15:57:08 +0000

    Moving Views

---
+++ MarionetteBackbone/Marionette/assets/js/apps/contacts/list/list_view.js
@@ -0,0 +1,11 @@
+﻿ContactManager.module("ContactsApp.List", function (List, ContactManager, Backbone, Marionette, $, _) {
+  List.Contact = Marionette.ItemView.extend({
+    template: "#contact-list-item",
+    tagName: "li"
+  });
+
+  List.Contacts = Marionette.CollectionView.extend({
+    tagName: "ul",
+    childView: List.Contact
+  });
+})
