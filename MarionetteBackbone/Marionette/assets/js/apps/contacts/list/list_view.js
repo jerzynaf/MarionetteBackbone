d7db0500d73b9ab9a33ed1f249adb1100195f945
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-12 11:04:18 +0000

    Delete button event handler added.

--- MarionetteBackbone/Marionette/assets/js/apps/contacts/list/list_view.js
+++ MarionetteBackbone/Marionette/assets/js/apps/contacts/list/list_view.js
@@ -1,13 +1,22 @@
 ﻿ContactManager.module("ContactsApp.List", function (List, ContactManager, Backbone, Marionette, $, _) {
   List.Contact = Marionette.ItemView.extend({
     template: "#contact-list-item",
-    tagName: "tr"
+    tagName: "tr",
+    events: {
+      "click": "highlightName",
+      "click button": function () {
+        alert("delete button was clicked");
+      }
+    },
+    highlightName: function () {
+      this.$el.toggleClass("warning");
+    }
   });
 
   List.Contacts = Marionette.CompositeView.extend({
     tagName: "table",
     className: "table table-hover",
-    template:"#contact-list",
+    template: "#contact-list",
     childView: List.Contact,
     childViewContainer: "tbody"
   });
