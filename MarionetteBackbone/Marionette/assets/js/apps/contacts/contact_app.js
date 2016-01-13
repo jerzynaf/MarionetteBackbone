5525bd2e3cf5bf102d6d312871324ea374b22a59
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-13 14:24:19 +0000

    Routes with ids added.

--- MarionetteBackbone/Marionette/assets/js/apps/contacts/contact_app.js
+++ MarionetteBackbone/Marionette/assets/js/apps/contacts/contact_app.js
@@ -1,15 +1,25 @@
 ﻿ContactManager.module("ContactsApp", function (ContactsApp, ContactManager, Backbone, Marionette, $, _) {
   ContactsApp.Router = Marionette.AppRouter.extend({
     appRoutes: {
-      "contacts": "listContacts"
+      "contacts": "listContacts",
+      "contacts/:id": "showContact"
     }
   });
 
   var API = {
     listContacts: function () {
-      console.log("route to list contacts was triggered");
+      ContactsApp.List.Controller.listContacts();
+    },
+
+    showContact: function(id) {
+      ContactsApp.Show.Controller.showContact(id);
     }
   };
+
+  ContactManager.on("contacts:list", function () {
+    ContactManager.navigate("contacts");
+    API.listContacts();
+  });
 
   ContactManager.addInitializer(function () {
     new ContactsApp.Router({
--- MarionetteBackbone/Marionette/assets/js/apps/contacts/show/show_controller.js
+++ MarionetteBackbone/Marionette/assets/js/apps/contacts/show/show_controller.js
@@ -1,7 +1,11 @@
 ﻿ContactManager.module("ContactsApp.Show", function (Show, ContactManager, Backbone, Marionette, $, _) {
   Show.Controller = {
-    showContact: function (model) {
-      var contactView = new ContactManager.ContactsApp.Show.Contact({ model: model });
+    showContact: function (id) {
+      var contacts = ContactManager.request("contact:entities");
+      var model = contacts.get(id);
+      var contactView = new Show.Contact({
+        model:model
+      });
       ContactManager.mainRegion.show(contactView);
     }
 
